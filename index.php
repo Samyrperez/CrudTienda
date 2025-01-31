ad576a6c0038d802dca9badcbc4068a9c409ad95
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-01-31 17:51:22 -0500

    Creacion de formulario doc index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="title">
+        <h2>Registro de usuarios</h2>
+    </div>
+
+    <div id="registroForm">
+    <input type="text" name="nombre" placeholder="Nombre" required><br>
+        <input type="email" name="email" placeholder="Correo Electrónico" required><br>
+        <input type="password" name="password" placeholder="Contraseña" required><br>
+        <button type="submit">Registrarse</button>
+    </div>
+
+    <div id="mensaje"></div>
+
+
+</body>
+</html>
